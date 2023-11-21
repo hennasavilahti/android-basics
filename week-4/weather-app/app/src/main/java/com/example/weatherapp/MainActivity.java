22ab8bdf5068abbb4b0df1fd07ea67ef0d094ad2
hennasavilahti <savilahtihenna@gmail.com>
2023-11-21 16:21:18 +0200

    Finished week 4 assignments

--- week-4/weather-app/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ week-4/weather-app/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,6 +15,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,15 +29,45 @@ public class MainActivity extends AppCompatActivity {
 
     public void getWeatherData(View view) {
         // Haetaan säätiedot Volley - kirjastosta
-        // RequestQueue queue = Volley.newRequestQueue(this);
-        String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=tampere&appid=6018b2f91594758c9da952d55a649364&units=metric";
-        StringRequest request = new StringRequest(Request.Method.GET, WEATHER_URL, response -> {
-            // Säätiedot haettu onnistuneesti. Parsitaan JSON-objekti ja
-        }, error -> {
+        String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=tampere&appid=6018b2f91594758c9da952d55a649364&units=metric";
+        // Säätiedot haettu onnistuneesti. Parsitaan JSON-objekti ja laitetaan käyttöliittymään haetut tiedot
+        StringRequest request = new StringRequest(Request.Method.GET, WEATHER_URL, this::parseWeatherJsonAndUpdateUi, error -> {
             // Verkkovirhe yms.
+            Toast.makeText(this, "VERKKOVIRHE", Toast.LENGTH_LONG).show();
         });
         // Lähetetään request Volleylla == Lisätään request requestqueueen
         Volley.newRequestQueue(this).add(request);
-        // Tämän jälkeen tullaan jompaan kumpaan callbacking haaraan, kun request on valmis
+    }
+
+    private void parseWeatherJsonAndUpdateUi(String response) {
+        // Parsitaan JSON ja päivitetään näytölle lämpötila, säätila ja tuulen nopeus
+        // Muunnetaan merkkijono JSON objektiksi
+        try {
+            JSONObject weatherJSON = new JSONObject(response);
+            String weather = weatherJSON.getJSONArray("weather").getJSONObject(0).getString("main");
+            double temperature = weatherJSON.getJSONObject("main").getDouble("temp");
+            double wind = weatherJSON.getJSONObject("wind").getDouble("speed");
+
+            TextView weatherTextView = findViewById(R.id.weatherTextView);
+            weatherTextView.setText(weather);
+            TextView temperatureTextView = findViewById(R.id.temperatureTextView);
+            temperatureTextView.setText("" + temperature + "C");
+            TextView windTextView = findViewById(R.id.windTextView);
+            windTextView.setText("" + wind + " m/s");
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void openWebPage(View view) {
+        String foreca = "https://foreca.fi";
+        Uri forecaUri = Uri.parse(foreca);
+        Intent intent = new Intent(Intent.ACTION_VIEW, forecaUri);
+        try {
+            startActivity(intent);
+        }
+        catch (Exception e) {
+        }
     }
 }
